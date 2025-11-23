cae5be4cbeb7f1d82237000b8ecf69d9f65f9fe3
william samwaka <williamsamwaka05@gmail.com>
2025-11-23 04:30:05 +0200

    Fixed test failure

--- src/main.c
+++ src/main.c
@@ -3,11 +3,19 @@
 #include <string.h>
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  
+  setbuf(stdout, NULL);
 
   char command[1024];
-  fgets(command,sizeof(command),stdin);
-  printf("%s: command not found\n",command);
+
+  while(1){
+    printf("$ ");
+    if(fgets(command,sizeof(command),stdin) == NULL){
+      break;
+    }
+    command[strcspn(command,"\n")] = "\0";
+
+   printf("%s: command not found\n",command);
+  } 
 
   return 0;
 }
