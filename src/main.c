ab6215f4d121efa745b0265a816c8e7dc334711a
william samwaka <williamsamwaka05@gmail.com>
2025-11-23 04:18:53 +0200

    making 'command not found message'

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  setbuf(stdout, NULL);
+  
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  char command[1024];
+  fgets(command,sizeof(command),stdin);
+  printf("%s: command not found\n",command);
 
   return 0;
 }
